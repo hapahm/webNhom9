5a3b03dc0c00fa2af4f91f7b787a19de4839b754
Pham Ha <minhha922004@gmail.com>
2024-10-26 19:36:08 +0700

    video 18

--- index.php
+++ index.php
@@ -152,28 +152,35 @@ require_once('header.php');
         <div class="row">
             <div class="col-lg-12">
                 <div class="section-title from-blog__title">
-                    <h2>From The Blog</h2>
+                    <h2>Tin tức</h2>
                 </div>
             </div>
         </div>
         <!-- yô các bài -->
         <div class="row">
             <!-- yô từng bài -->
-            <div class="col-lg-4 col-md-4 col-sm-6">
-                <div class="blog__item">
-                    <div class="blog__item__pic">
-                        <img src="img/blog/blog-1.jpg" alt="">
-                    </div>
-                    <div class="blog__item__text">
-                        <ul>
-                            <li><i class="fa fa-calendar-o"></i> May 4,2019</li>
-                            <li><i class="fa fa-comment-o"></i> 5</li>
-                        </ul>
-                        <h5><a href="#">Cooking tips make cooking simple</a></h5>
-                        <p>Sed quia non numquam modi tempora indunt ut labore et dolore magnam aliquam quaerat </p>
+            <?php
+            $sql_str = "select * from news order by created_at desc limit 0, 3";
+            $result = mysqli_query($conn, $sql_str);
+            while ($row = mysqli_fetch_assoc($result)) {
+
+                ?>
+                <div class="col-lg-4 col-md-4 col-sm-6">
+                    <div class="blog__item">
+                        <div class="blog__item__pic">
+                            <img src="<?= 'quantri/' . $row['avatar'] ?>" alt="">
+                        </div>
+                        <div class="blog__item__text">
+                            <ul>
+                                <li><i class="fa fa-calendar-o"></i> <?= $row['created_at'] ?></li>
+                                <li><i class="fa fa-comment-o"></i> 5</li>
+                            </ul>
+                            <h5><a href="tintuc.php?id=<?= $row['id'] ?>"><?= $row['title'] ?></a></h5>
+                            <p><?= $row['sumary'] ?></p>
+                        </div>
                     </div>
                 </div>
-            </div>
+            <?php } ?>
 
 
         </div>
